7da5a64a68a6d629c568add2b07600ecdd8f257a
Daniel Jendinger <danne@codeland.se>
2025-06-01 13:59:59 +0200

    Small edit..

--- MAKE_ALL.sh
+++ MAKE_ALL.sh
@@ -1,8 +1,8 @@
 cd tbar_source
 cc tbar.c -o tbar
 cc tline.c -o tline
-cp tbar /usr/local/bin/
-cp tline /usr/local/bin/
+mv tbar /usr/local/bin/
+mv tline /usr/local/bin/
 chmod +x /usr/local/bin/tbar
 chmod +x /usr/local/bin/tline
 rm tbar
@@ -10,7 +10,7 @@ rm tline
 cd ..
 cd tdraw_source
 cc tdraw.c -o tdraw
-cp tdraw /usr/local/bin/
+mv tdraw /usr/local/bin/
 chmod +x /usr/local/bin/tdraw
 rm tdraw
 
@@ -18,8 +18,8 @@ cd ..
 cd tcolor_source 
 cc tcolor.c -o tcolor
 cc tcolorl.c -o tcolorl
-cp tcolor /usr/local/bin/
-cp tcolorl /usr/local/bin/
+mv tcolor /usr/local/bin/
+mv tcolorl /usr/local/bin/
 chmod +x /usr/local/bin/tcolor
 chmod +x /usr/local/bin/tcolorl
 rm tcolor
