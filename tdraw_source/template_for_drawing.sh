5a95026531f87868e3cf0d5d5f57cbbaad4fba68
Daniel Jendinger <danne@codeland.se>
2025-06-01 14:09:14 +0200

    Small edit..

---
+++ tdraw_source/template_for_drawing.sh
@@ -0,0 +1,29 @@
+##########################################
+#       TermTools Template tdraw         #
+#                                        #
+#    2013 hexabit  (Daniel Jendinger)    #
+#                                        #
+#           www.codeland.se              #
+##########################################
+clear
+echo " "
+echo " Her is a template to start drawing: "
+echo "    1 for filling with blue etc "
+echo " See the colorbar for color examples"
+echo ""
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+tdraw 00000000000000000000000000000000000.
+echo " "
+echo "    Blocks with colors:"
+echo " "
+tdraw 00111222333444566777888.
+echo " "
